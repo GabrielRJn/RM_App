3c849356dc6d86f00bca5ef7282221d8bda36551
GabrielRJn <gabrielrainajohnson@gmail.com>
2022-10-11 18:16:34 +0100

    GUI fix
    GUI wasn't appearing so re-did it

--- RM App/RM App/Form1.cs
+++ RM App/RM App/Form1.cs
@@ -16,5 +16,20 @@ namespace RM_App
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
